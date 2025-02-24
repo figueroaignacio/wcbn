f0a78414dcec1597c8fbce0140095943768546be
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-24 14:39:27 -0300

    feat: add challenges section

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,5 @@
 // express
+import cors from "cors";
 import express, { Request, Response } from "express";
 
 // Routes
@@ -7,6 +8,13 @@ import challengeRouter from "./routes/challenge.route";
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "UPDATE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`
