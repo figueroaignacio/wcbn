8a23cb006b3c7d7a5261d1e5948631efaba378a0
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-24 15:31:14 -0300

    feat: add title and description

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,6 +3,13 @@ import { Challenges } from "./sections/challenges/challenges";
 export default function App() {
   return (
     <main>
+      <div>
+        <h1>Web Challenges by Nacho</h1>
+        <p>
+          This site showcases the web challenges I completed throughout my
+          journey.
+        </p>
+      </div>
       <Challenges />
     </main>
   );
