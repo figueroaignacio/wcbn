0f5592880bce62abcd987d53cbe5a0123cdb7cc7
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-25 16:56:22 -0300

    feat: add seo component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,16 @@
 import styles from "./app.module.css";
 import { Header } from "./components/header/header";
+import { Seo } from "./components/seo";
 import { Challenges } from "./sections/challenges/challenges";
 import { Hero } from "./sections/hero/hero";
 
 export default function App() {
   return (
     <div>
+      <Seo
+        title="WCBN - Completed Challenges Collection"
+        description="Fullstack site that showcases my completed web challenges"
+      />
       <Header />
       <main className={styles.app}>
         <Hero />
