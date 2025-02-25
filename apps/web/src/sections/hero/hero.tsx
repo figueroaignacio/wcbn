5aca89cfa44c39796075d716fca1d560257433f9
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-25 14:05:52 -0300

    style: hero

--- apps/web/src/sections/hero/hero.tsx
+++ apps/web/src/sections/hero/hero.tsx
@@ -1,18 +1,17 @@
+import { ArrowDownIcon } from "@radix-ui/react-icons";
 import styles from "./hero.module.css";
 
 export function Hero() {
   return (
     <section className={styles.hero}>
-      <img
-        src="https://giffiles.alphacoders.com/215/215911.gif"
-        alt="gif"
-        className={styles.heroGif}
-      />
-      <h1 className={styles.heroTitle}>Web Challenges by Nacho</h1>
-      <p className={styles.heroDescription}>
+      <h1>Web Challenges by Nacho</h1>
+      <p>
         This site showcases the web challenges I completed throughout my
         journey.
       </p>
+      <div className={styles.arrowWrapper}>
+        <ArrowDownIcon className={styles.arrow} width={34} height={34} />
+      </div>
     </section>
   );
 }
